0b80c5937e2a450d45f093118f3451dc20107328
WAKAM1258 <laurent.bastien69@gmail.com>
2026-03-15 17:05:44 +0000

    Fix: Hardcode production env in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Cela force React à ne pas chercher les fichiers de production bizarres
+    'process.env.NODE_ENV': JSON.stringify('production')
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
+    }
+  }
 })
-// force deploy 1
