47b771cbecb645e15dae0f8cff3b629357fc2ae9
WAKAM1258 <laurent.bastien69@gmail.com>
2026-03-15 17:18:29 +0000

    Fix: Simplified Vite config and cleaned main.jsx

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // Cela force React à ne pas chercher les fichiers de production bizarres
-    'process.env.NODE_ENV': JSON.stringify('production')
-  },
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/],
-      transformMixedEsModules: true
-    }
-  }
-})
+})
